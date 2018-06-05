ddfc96c9cd56b19acde286ed4533bbe89f21b561
MacroLions <00175617@uca.edu.sv>
2018-06-05 12:13:48 -0600

    Jugador, los comienzos.

---
+++ src/Parcial_2_POO/Jugador.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parcial_2_POO;
+
+import Edificacion.Edificacion;
+import Razas.Raza;
+import Tropa.Tropa;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mai Perez
+ */
+public class Jugador {
+    private Raza raza;
+    private int recurso_1;
+    private int recurso_2;
+    private int recurso_3;
+    private final ArrayList<Tropa> Tropas = new ArrayList();
+    private final ArrayList<Edificacion> Edificaciones = new ArrayList();
+    
+    public void Construir(){};
+    public void Atacar(){};
+    public void Recolectar(){};
+    
+    
+}
