63b5df476fa4e3312231196c230e0463bc24c33d
MacroLions <00175617@uca.edu.sv>
2018-06-05 15:32:38 -0600

    Se pueden construir (SIN GASTOS AÚN) las 3 maquinas de recurso además de recolectar de ellas.

--- src/Parcial_2_POO/Jugador.java
+++ src/Parcial_2_POO/Jugador.java
@@ -19,9 +19,9 @@ import java.util.Scanner;
 public class Jugador {
     private final String nombre;
     private Raza raza;
-    private final int OroTotal=1000;
-    private final int MagiaTotal=1000;
-    private final int Diamantes=1;
+    private int OroTotal=1000;
+    private int MagiaTotal=1000;
+    private int Diamantes=1;
     private final ArrayList<Tropa> Tropas = new ArrayList();
     private final ArrayList<Edificacion> Edificaciones = new ArrayList();
     
@@ -33,10 +33,19 @@ public class Jugador {
     
     public void MenuJugador(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Hola "+this.nombre+" ¿Que quieres en este turno?");
+        Auxiliar.RecursosCreatorMaster(Edificaciones, EdificacionFactory.getEdificacion(1));
+        Auxiliar.RecursosCreatorMaster(Edificaciones, EdificacionFactory.getEdificacion(2));
+        Auxiliar.RecursosCreatorMaster(Edificaciones, EdificacionFactory.getEdificacion(3));
+        
+        System.out.println("////Hola "+this.nombre+" ¿Que quieres en este turno?////");
+        System.out.println("Recursos disponibles: Oro: "+this.OroTotal+" Magia: "+this.MagiaTotal+" Diamantes: "+this.Diamantes);
         System.out.println("1. Construir");
         System.out.println("2. Recolectar Recursos");
+        
+        System.out.print("Opcion: ");
         int op = input.nextInt();
+        System.out.println("");
+        
         switch(op){
             case 1:
                 Construir();
@@ -49,24 +58,67 @@ public class Jugador {
     
     public void Construir(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Construir?");
-        System.out.println("1. Maquina de Diamantes");
+        System.out.println("Construir:");
+        System.out.println("1. Maquina de Oro");
+        System.out.println("2. Maquina de Magia");
+        System.out.println("3. Maquina de Diamantes");
+        
+        System.out.print("Opcion: ");
         int op = input.nextInt();
+        System.out.println("");
+        
         switch(op){
             case 1:
                 Edificaciones.add(EdificacionFactory.getEdificacion(op));
+                System.out.println("Se construyó maquina de Oro");
+                break;
+            case 2:
+                Edificaciones.add(EdificacionFactory.getEdificacion(op));
+                System.out.println("Se construyó maquina de Magia");
+                break;
+            case 3:
+                Edificaciones.add(EdificacionFactory.getEdificacion(op));
+                System.out.println("Se construyó maquina de diamantes");
+                break;
         }
-        System.out.println("");
         
     };
+    
+    
     public void RecolectarRecursos(){
+        Scanner input = new Scanner(System.in);
         if(Edificaciones.isEmpty()==false){
             System.out.println("Maquinas disponibles:");
             int Comprobador_1 = Auxiliar.RevisarEdificaciones(Edificaciones, EdificacionFactory.getEdificacion(1));
+            int Comprobador_2 = Auxiliar.RevisarEdificaciones(Edificaciones, EdificacionFactory.getEdificacion(2));
+            int Comprobador_3 = Auxiliar.RevisarEdificaciones(Edificaciones, EdificacionFactory.getEdificacion(3));
+            
             if(Comprobador_1!=0){
-                System.out.println("Maquina de Diamantes x"+Comprobador_1);
+                System.out.println("Maquina de Oro x"+Comprobador_1);
+            }
+            if(Comprobador_2!=0){
+                System.out.println("Maquina de Magia x"+Comprobador_2);
+            }
+            if(Comprobador_3!=0){
+                System.out.println("Maquina de Diamantes x"+Comprobador_3);
+            }
+            
+            if(Comprobador_1==0&&Comprobador_2==0&&Comprobador_3==0){
+                System.out.println("No hay Maquinas de recursos, solo otro tipo de edificaciones.");
+            }
+            if(Comprobador_1!=0||Comprobador_2!=0||Comprobador_3!=0){
+                this.OroTotal = this.OroTotal + Auxiliar.RecolectorMaster(Edificaciones, EdificacionFactory.getEdificacion(1));
+                this.MagiaTotal = this.MagiaTotal + Auxiliar.RecolectorMaster(Edificaciones, EdificacionFactory.getEdificacion(2));
+                this.Diamantes = this.Diamantes + Auxiliar.RecolectorMaster(Edificaciones, EdificacionFactory.getEdificacion(3));
+                System.out.println("Se ha recolectado todo lo posible");
+                
             }
         }
+        
+        else if(Edificaciones.isEmpty()){
+                System.out.println("No hay edificaciones");    
+        }
+           
     };
     
     public void Atacar(){};
