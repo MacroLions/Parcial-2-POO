cd86f6d3746dc5290eba5a07c823d02947e3b4f4
MacroLions <00175617@uca.edu.sv>
2018-06-05 12:11:25 -0600

    Recurso 2: Magia

---
+++ src/Edificacion/MaquinadeMagia.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Edificacion;
+import java.util.Date;
+
+/**
+ *
+ * @author Mai Perez
+ */
+public abstract class MaquinadeMagia implements Edificacion {
+    int Vida=1000;
+    int RecursosGuardados;
+    int tiempoAnterior=0;
+    
+    @Override
+    public void GenerarRecursos() {
+         if(Vida!=0){
+            int segundosActuales = new Date().getSeconds();
+        
+            if(tiempoAnterior==0){
+                this.tiempoAnterior=segundosActuales;
+            }
+        
+            this.RecursosGuardados= this.RecursosGuardados+ (segundosActuales-tiempoAnterior)*10;
+        }
+    }
+
+    @Override
+    public int EntregarRecursos() {
+        if(RecursosGuardados>0){
+            int Entregado = (int) this.RecursosGuardados;
+            this.RecursosGuardados=0;
+            return Entregado; 
+        }
+        else{
+            System.out.println("No hay magia");
+            return 0;
+        }
+    }
+    
+    @Override
+    public void RecibirDamage(int damage){
+        this.Vida=this.Vida-damage;
+    };
+
+    
+}
