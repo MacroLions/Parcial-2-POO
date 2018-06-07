583d7f19fcd1678c5ab6d148785e0800313b1dc3
MacroLions <00175617@uca.edu.sv>
2018-06-07 11:18:26 -0600

    Fabrica de Tropas

---
+++ src/Tropa/TropaFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tropa;
+
+/**
+ *
+ * @author Mai Perez
+ */
+public class TropaFactory {
+     public static Tropa getTropa(int opcion) {
+        switch(opcion){
+            case 1:
+                return new Escuadron();
+                
+            case 2:
+                return new SuperSoldado();
+
+        }
+        return null;
+    }
+    
+}
