510eebe115c9b64369b67ecd8b8c76e8a99a5c95
MacroLions <00175617@uca.edu.sv>
2018-06-09 12:34:38 -0600

    Ahora ejecuta una funcion static para no tener más modificaciones. Se espera no tener que modificar más el main por ahora.

--- src/Parcial_2_POO/Parcial_2_POO.java
+++ src/Parcial_2_POO/Parcial_2_POO.java
@@ -15,31 +15,7 @@ public class Parcial_2_POO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          
-        Jugador Mai = new Jugador("Mai");
-        Jugador Mako = new Jugador("Mako");
-        boolean TurnoJugador1= true;
-        boolean TurnoJugador2= true;
-        
-        boolean Start = true;
-        
-        while(Start){
-            System.out.println("Fase #"+Auxiliar.getFase());
-            while(TurnoJugador1){
-                TurnoJugador1= Mai.MenuJugador();
-                System.out.println("");
-            }
-            
-            /*
-            while(TurnoJugador2){
-                TurnoJugador2= Mako.MenuJugador();
-                System.out.println("");
-            }
-            */
-            Auxiliar.setFase(Auxiliar.getFase()+1);
-            TurnoJugador1=true;
-            TurnoJugador2=true;
-        }
+        MenuInicial.MenuInicio();
       
     }
     
