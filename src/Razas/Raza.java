63cafcff08e06e0fc484c59cba0b5542453cecb2
MacroLions <00175617@uca.edu.sv>
2018-06-12 13:18:21 -0600

    Gets y Sets generales para las razas.

--- src/Razas/Raza.java
+++ src/Razas/Raza.java
@@ -10,7 +10,35 @@ package Razas;
  * @author Maishi
  */
 public interface Raza {
-    public void Construir();
-    public void Atacar();
-    public void Defender();
+    public String getNombreRecurso1();
+    public void setNombreRecurso1(String NombreRecurso1);
+    public String getNombreRecurso2();
+    public void setNombreRecurso2(String NombreRecurso2);
+    public String getNombreRecurso3();
+    public void setNombreRecurso3(String NombreRecurso3);
+
+
+    public String getNombreMaquinaRecurso1();
+    public void setNombreMaquinaRecurso1(String NombreMaquinaRecurso1);
+    public String getNombreMaquinaRecurso2();
+    public void setNombreMaquinaRecurso2(String NombreMaquinaRecurso2);
+    public String getNombreMaquinaRecurso3();
+    public void setNombreMaquinaRecurso3(String NombreMaquinaRecurso3);
+
+
+    public String getNombreEscuadron();
+    public void setNombreEscuadron(String NombreEscuadron);
+    public int getAtaqueEscuadron();
+    public void setAtaqueEscuadron(int AtaqueEscuadron);
+    public int getEsperaEscuadron();
+    public void setEsperaEscuadron(int EsperaEscuadron);
+    
+
+
+    public String getNombreSuperSoldado();
+    public void setNombreSuperSoldado(String NombreSuperSoldado);
+    public int getAtaqueSuperSoldado();
+    public void setAtaqueSuperSoldado(int AtaqueSuperSoldado);
+    public int getEsperaSuperSoldado();
+    public void setEsperaSuperSoldado(int SuperSoldado);
 }
