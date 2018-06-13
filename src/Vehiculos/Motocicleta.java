0644803ed05a830b715c7c558fc737d8fefcef61
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-06-13 12:57:57 -0600

    Class Moticicleta creado!

---
+++ src/Vehiculos/Motocicleta.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vehiculos;
+
+import Parcial_2_POO.Jugador;
+
+/**
+ *
+ * @author Maishi
+ */
+public class Motocicleta implements Vehiculo {
+    Jugador Propietario;
+    private String Nombre = "Motocicleta";
+    private boolean Disponibilidad = true;
+
+    public Jugador getPropietario() {
+        return Propietario;
+    }
+
+    public void setPropietario(Jugador Propietario) {
+        this.Propietario = Propietario;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public boolean isDisponibilidad() {
+        return Disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean Disponibilidad) {
+        this.Disponibilidad = Disponibilidad;
+    }
+    
+    
+    
+}
