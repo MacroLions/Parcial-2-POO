94bff3b1777f5c085c11bc44ae4e649ef4c8a84b
MacroLions <37413420+MacroLions@users.noreply.github.com>
2018-06-12 22:53:57 -0600

    VehiculoFactory agregada. Aun no devuelve Vehiculos

---
+++ src/Vehiculos/VehiculoFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vehiculos;
+
+
+/**
+ *
+ * @author Mai Perez
+ */
+public class VehiculoFactory {  
+    public static Vehiculo getVehiculo(int opcion) {
+        switch(opcion){
+            case 1:
+                break;
+        }
+        return null;
+    }
+    
+}
