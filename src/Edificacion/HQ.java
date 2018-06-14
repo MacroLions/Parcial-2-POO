90c0959a719f4b0613aaec210271b17c94d71b89
MacroLions <00175617@uca.edu.sv>
2018-06-14 16:56:44 -0600

    Limites de recursos establecidos.

--- src/Edificacion/HQ.java
+++ src/Edificacion/HQ.java
@@ -19,8 +19,22 @@ public class HQ implements Edificacion{
     private int Vida=10000;
     private boolean Vivo=true;
     private int RecursoTotal1=2000;
+    private int LimiteRecurso1=10000;
     private int RecursoTotal2=2000;
+    private int LimiteRecurso2=5000;
     private int RecursoTotal3=20;
+    private int LimiteRecurso3=3000;
+
+    @Override
+    public void RecibirDamage(int damage) {
+        System.out.println("El HQ de "+this.propitario.getNombre()+" recibio "+damage+" de daño");
+        Vida=Vida-damage;
+        if(Vida<=0){
+            Vivo=false;
+            //destroy();
+        }
+        
+    }
 
     public String getNombre() {
         return Nombre;
@@ -62,6 +76,14 @@ public class HQ implements Edificacion{
         this.RecursoTotal1 = RecursoTotal1;
     }
 
+    public int getLimiteRecurso1() {
+        return LimiteRecurso1;
+    }
+
+    public void setLimiteRecurso1(int LimiteRecurso1) {
+        this.LimiteRecurso1 = LimiteRecurso1;
+    }
+
     public int getRecursoTotal2() {
         return RecursoTotal2;
     }
@@ -70,12 +92,28 @@ public class HQ implements Edificacion{
         this.RecursoTotal2 = RecursoTotal2;
     }
 
+    public int getLimiteRecurso2() {
+        return LimiteRecurso2;
+    }
+
+    public void setLimiteRecurso2(int LimiteRecurso2) {
+        this.LimiteRecurso2 = LimiteRecurso2;
+    }
+
     public int getRecursoTotal3() {
         return RecursoTotal3;
     }
 
     public void setRecursoTotal3(int RecursoTotal3) {
         this.RecursoTotal3 = RecursoTotal3;
+    }
+
+    public int getLimiteRecurso3() {
+        return LimiteRecurso3;
+    }
+
+    public void setLimiteRecurso3(int LimiteRecurso3) {
+        this.LimiteRecurso3 = LimiteRecurso3;
     }
 
     @Override
@@ -99,17 +137,6 @@ public class HQ implements Edificacion{
     }
 
     @Override
-    public void RecibirDamage(int damage) {
-        System.out.println("El HQ de "+this.propitario.getNombre()+" recibio "+damage+" de daño");
-        Vida=Vida-damage;
-        if(Vida<=0){
-            Vivo=false;
-            //destroy();
-        }
-        
-    }
-
-    @Override
     public void run() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -118,6 +145,7 @@ public class HQ implements Edificacion{
     public void start() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
+    
 
 }
